84e22db2371f0495b57633e417c6410e17dd2603
cesarlopezt <cesarlopez2808@gmail.com>
2022-10-25 08:27:38 -0400

    Initial Commit

---
+++ RandomCard-Programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomCard-Programmatic
+//
+//  Created by Cesar Lopez on 10/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RandomCard-ProgrammaticUITests/RandomCard_ProgrammaticUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RandomCard_ProgrammaticUITestsLaunchTests.swift
+//  RandomCard-ProgrammaticUITests
+//
+//  Created by Cesar Lopez on 10/25/22.
+//
+
+import XCTest
+
+final class RandomCard_ProgrammaticUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
